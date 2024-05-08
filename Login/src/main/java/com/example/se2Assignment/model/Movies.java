5fb8202d7c9ceda582bf0e9cd7b4e8d8c7427efe
Minh Vu Nguyen <vuoccho070803@gmail.com>
2024-05-08 12:07:24 +0700

    Adding movies and theater table

---
+++ Login/src/main/java/com/example/se2Assignment/model/Movies.java
@@ -0,0 +1,97 @@
+package com.example.se2Assignment.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movies {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String movieName;
+    private String genre;
+    private String cast;
+    private String duration;
+
+    private String rate;
+    private String posterUrl;
+    private String description;
+    public Movies(long id, String movieName, String genre, String cast, String duration,
+                   String rate, String posterUrl, String description) {
+        this.id = id;
+        this.movieName = movieName;
+        this.genre = genre;
+        this.cast = cast;
+        this.duration = duration;
+
+        this.rate = rate;
+        this.posterUrl = posterUrl;
+        this.description = description;
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+
+
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Login/src/main/java/com/example/se2Assignment/repository/MoviesRepository.java
@@ -0,0 +1,8 @@
+package com.example.se2Assignment.repository;
+
+import com.example.se2Assignment.model.Movies;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoviesRepository  extends JpaRepository<Movies,Long> {
+
+}
