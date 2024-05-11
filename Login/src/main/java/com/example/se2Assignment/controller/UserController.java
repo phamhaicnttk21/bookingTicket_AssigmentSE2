a3ebb21a9c69296fe048e354e08f111bf5253d42
Minh Vu Nguyen <vuoccho070803@gmail.com>
2024-05-12 01:40:11 +0700

    a commit

--- Login/src/main/java/com/example/se2Assignment/controller/UserController.java
+++ Login/src/main/java/com/example/se2Assignment/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.se2Assignment.controller;
 
 import com.example.se2Assignment.dto.UserDto;
+import com.example.se2Assignment.model.Movie;
+import com.example.se2Assignment.service.MovieService;
 import com.example.se2Assignment.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class UserController {
